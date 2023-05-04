401f912916d92827ad124af785b923aed2e8c7d5
Devoxys <davekpeng@gmail.com>
2023-05-04 11:36:48 -0400

    fixing spacing

--- pokepuzzleladder/src/components/HeadRow.js
+++ pokepuzzleladder/src/components/HeadRow.js
@@ -2,7 +2,7 @@ const HeadRow = ({ head }) => {
     return (
         <div className='head'>
             {head.map((num, i) =>
-                <div key={i}>{num}</div> 
+                <div key={i} className="head-cell">{num}</div> 
             )}
         </div>
     )
